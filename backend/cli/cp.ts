9df7b01cb7923608bd9032997da55ba2fe99ebfa
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-02 13:45:45 +0100

    page create in oop finished

--- backend/cli/cp.ts
+++ backend/cli/cp.ts
@@ -5,7 +5,6 @@ const pageIdCode = tools.getArgument(0);
 
 try {
 	const pageManager = new PageManager(pageIdCode)
-	// pageManager.debug();
 	pageManager.createPage();
 }
 catch (e) {
@@ -15,19 +14,3 @@ Syntax: npm run cp <pageIdCode>
 Example: npm run cp Reports	
 `.trim());
 }
-
-
-// // const option = tools.getArgument(1);
-
-// console.log(`Creating ${pageFileName}...`);
-
-
-/*
-process.argv 
----------------------------
-0: ...node.exe
-1: ...cp.ts
-2: Reports
-3: flat
-
-*/
