c4292966e64da1aeb818d5b2f48d5d10af416537
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-02 14:18:00 +0100

    debugging lines

--- backend/cli/debug.ts
+++ backend/cli/debug.ts
@@ -1,4 +1,5 @@
 import { DynamicFile } from "./DynamicFile";
 
 const dynamicFile = new DynamicFile('src/components/Nav.tsx');
+// dynamicFile.addStringBlockBeforeMarker('</ul>', 'test line');
 dynamicFile.debug();
